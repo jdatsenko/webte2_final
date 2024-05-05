940497b9e983a3d90e127861cd707b1c0534c475
Yuliia Datsenko <xdatsenko@stuba.sk>
2024-05-05 19:32:57 +0200

    empty assets outDir

---
+++ src/assets/Home-DX5vmnuL.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index-DBezcG-W.js";const r={};function c(o,t){return" Home "}const n=e(r,[["render",c]]);export{n as default};
--- src/frontend/vite.config.js
+++ src/frontend/vite.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { resolve } from 'path'
+import { rm } from 'node:fs/promises'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), {
+    name: "Cleaning assets folder",
+    async buildStart() {
+      await rm(resolve(__dirname, '../assets'), { recursive: true, force: true });
+    }
+  }],
   build: {
     outDir: "../",
   },
