5ce6916dda8479f8edd9022b07ad71f60d8485e5
Yuliia Datsenko <xdatsenko@stuba.sk>
2024-05-05 19:11:53 +0200

    vue router

---
+++ src/frontend/src/router.js
@@ -0,0 +1,18 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+export const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {
+        path: '/',
+        name: 'home',
+        component: () => import('./views/Home.vue')
+        },
+        {
+            path: '/login',
+            name: 'login',
+            component: () => import('./views/Login.vue')
+            },
+    ]
+
+})
