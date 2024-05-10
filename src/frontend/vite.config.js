5babf29044c79f7a662ed6e77d3d309a119c8f97
ihrow <ihrow@proton.me>
2024-05-10 13:41:13 +0200

    rename app

--- src/frontend/vite.config.js
+++ src/frontend/vite.config.js
@@ -1,23 +1,36 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import { resolve } from 'path'
-import { rm } from 'node:fs/promises'
+import { resolve } from "path";
+import { rm } from "node:fs/promises";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), {
-    name: "Cleaning assets folder",
-    async buildStart() {
-      await rm(resolve(__dirname, '../assets'), { recursive: true, force: true });
-    }
-  }],
+  plugins: [
+    vue(),
+    {
+      name: "Cleaning assets folder",
+      async buildStart() {
+        await rm(resolve(__dirname, "../assets"), {
+          recursive: true,
+          force: true,
+        });
+      },
+    },
+  ],
   build: {
     minify: false,
     outDir: "../",
+    rollupOptions: {
+      output: {
+        assetFileNames: () => {
+          return "assets/[name].[ext]";
+        },
+      },
+    },
   },
-  resolve: {  
+  resolve: {
     alias: {
-      '@': resolve(__dirname, './src')
-    }
-  }
+      "@": resolve(__dirname, "./src"),
+    },
+  },
 });
