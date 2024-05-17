ebb8437b26e2ec835560ecbdb2012327f88d9e6c
JakubBansky <jakub.bansky@gmail.com>
2024-05-17 13:12:19 +0200

    !pretty word cloud

--- src/frontend/src/views/Results.vue
+++ src/frontend/src/views/Results.vue
@@ -1,9 +1,11 @@
 <script setup>
-import { ref, onMounted, toRaw } from "vue";
+import { ref, onMounted, toRaw, computed } from "vue";
 import Chart from 'primevue/chart';
 import Fieldset from "primevue/fieldset";
 import { useQuestionStore } from '../stores/question.store';
 import { GetAllAnswers } from "../api";
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
 
 const currentPath = window.location.pathname;
 const parts = currentPath.split("/");
@@ -14,9 +16,11 @@ const chartData = ref();
 const chartOptions = ref();
 const showChoiceChart = ref(false);
 const showOpenChart = ref(false);
-const texts = ref({
-    texts: [],
-    answers: []
+const container = ref(null);
+
+
+const answers = ref({
+    answer: []
 })
 
 const setChartOptions = () => {
@@ -85,23 +89,66 @@ onMounted(async () => {
     state.answers.forEach(element => {
         labels.push(element.answer);
     });
+
     response.data.responses.forEach(item => {
         data.push(item.count);
-        texts.value.answers.push(item.count);
+        const answer = {
+            text: item.text,
+            count: item.count,
+        };
+        answers.value.answer.push(answer);
+
     });
 
     chartData.value = setChartData(labels, data, response.data.responses);
     chartOptions.value = setChartOptions();
-    if (state.question.type === "answer") {
-        response.data.responses.forEach(item => {
-            texts.value.texts.push(item.text);
-        });
-    }
+
+    answers.value.answer.sort((a, b) => b.count - a.count);
+
+    container.value = document.getElementById('container-id');
+
+   
+    populateWorldCloud();
+
 });
 
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+function populateWorldCloud() {
+    let j = answers.value.answer.length - 1;
+    for (let i = 1; i < answers.value.answer.length + 1; i++) {
+        const answer = answers.value.answer[j];
+        answer.fontSize = 10 + 5 * i;
+        j--;
+    }
+    shuffle(answers.value.answer);
+
+}
+const generateRandomColor = () => {
+    const red = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
+
+    const colorCode = `#${red.toString(16)}${green.toString(16)}${blue.toString(16)}`;
+
+    return colorCode;
+};
+
+function getRandomTextAlignment(index) {
+    const alignments = ['left', 'center', 'right'];
+    return alignments[Math.floor(Math.random() * alignments.length)];
+}
 </script>
 
 <template>
+    <v-list lines="one">
+        <v-list-item v-for="n in 3" :key="n" :title="'Item ' + n"
+            subtitle="Lorem ipsum dolor sit amet consectetur adipisicing elit"></v-list-item>
+    </v-list>
     <div class="flex justify-content-center">
         <Fieldset :legend="state.question.subject" style="width: 50%">
             <h1>{{ state.question.question }}</h1>
@@ -111,18 +158,26 @@ onMounted(async () => {
                 </div>
             </div>
             <div v-if="showOpenChart" class="flex justify-content-center">
-                <div class="flex justify-content-center">
-                    <ul>
-                        <li v-for="(text, index) in texts.texts" :key="'text-' + index">{{ text }}</li>
+                <Fieldset legend="Answers" style="width: 60%;">
+                    <ul style="list-style-type: none;">
+                        <li v-for="(answer, index) in answers.answer" :key="'answer-' + index">
+                            {{ answer.count }} - {{ answer.text }}
+                        </li>
                     </ul>
-                    <ul>
-                        <li v-for="(answer, index) in texts.answers" :key="'answer-' + index">{{ answer }}</li>
-                    </ul>
-                </div>
+
+                </Fieldset>
             </div>
-            <div v-else class="flex justify-content-center">
-                <div class="flex justify-content-center">
-                    
+            <div v-else class="flex">
+                <div style="display: grid;
+                    grid-template-columns: repeat(3, 1fr);
+                    gap: 10px;">
+                    <p v-for="(answer, index) in answers.answer" :key="'answer' + index" :style="{
+                        fontSize: answer.fontSize + 'px',
+                        color: generateRandomColor(),
+                        textAlign: getRandomTextAlignment(index),
+                        margin: 0,
+                        padding: 10 + 'px',
+                    }">{{ answer.text }}</p>
                 </div>
             </div>
         </Fieldset>
